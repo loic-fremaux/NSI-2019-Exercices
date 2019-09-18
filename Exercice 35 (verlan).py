6f80fe57e19d94c0a60a979092bebdd75a381414
Loïc <loic.fremaux1@gmail.com>
2019-09-18 21:35:32 +0200

    Changed loop index / Added comments

--- Exercice 35 (verlan).py
+++ Exercice 35 (verlan).py
@@ -1,8 +1,14 @@
 def verlan(string):
+    """
+    invert the content of a string
+
+    :param string: to invert
+    :return: inverted string
+    """
     result = ""
     length = len(string)
-    for i in range(length):
-        result += string[length - i - 1]
+    for i in range(1, length + 1):
+        result += string[length - i]
     return result
 
 
